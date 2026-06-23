afc0b25e4ad7dd6cb945487204a4bf928cdb87d5
Josefine <josefineeriksson@live.se>
2026-06-23 11:55:27 +0200

    portfolio

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -4,11 +4,12 @@ import ProjectCard from "./ProjectCard";
 import { useTranslate } from "@/locales/use-locales";
 
 const PROJECT_KEYS = [
+  "knegarloggen",
+    "kladerEfterVader",
   "bMovies",
   "dagensDons",
   "motionzone",
   "sweettimeUf",
-  "kladerEfterVader",
   "skeningeVvs",
   "vaderskuggan",
   "studenthubben",
@@ -22,76 +23,85 @@ const PROJECT_DATA: Record<
   string,
   { image: string; tech: string; projectLink?: string; codeLink?: string }
 > = {
+
+  knegarloggen: {
+    image: "/image/mockupbilder/knegarloggen.png",
+    tech: "React, Next.js, TypeScript, Tailwind CSS, Shadcn, PostgreSQL, Prisma, Zod, BetterAuth, API, Stripe, Nodmailer,",
+    codeLink: "https://github.com/knixan/knegarloggen",
+    projectLink: "https://knegarloggen.vercel.app/",
+  },
+
+    kladerEfterVader: {
+    image: "/image/mockupbilder/vaderefterklader.jpg",
+    tech: "React, Next.js, TypeScript, Tailwind CSS, OpenWeatherMap API",
+    projectLink: "https://vaderefterklader.se",
+    codeLink: "https://github.com/knixan/vaderefterklader",
+  },
+
   bMovies: {
-    image: "/Image/mockup-bmovies.jpg",
+    image: "/image/mockupbilder/bmovies.jpg",
     tech: "React, Next.js, TypeScript, Tailwind CSS, Shadcn, PostgreSQL, Prisma, Zod, BetterAuth, API",
     codeLink: "https://github.com/knixan/b-movies",
     projectLink: "https://b-movies.vercel.app/",
   },
   sweettimeUf: {
-    image: "/Image/mockup-sweettime-uf.png",
+    image: "/image/mockup-sweettime-uf.png",
     tech: "React, Next.js, TypeScript, Tailwind CSS, Prisma, PostgreSQL, Zod, BetterAuth, Shadcn",
     projectLink: "sweettimeuf-woad.vercel.app/",
     codeLink: "https://github.com/knixan/sweettimeuf",
   },
 
     motionzone: {
-    image: "/Image/mockupbilder/motionzone.jpg",
+    image: "/image/mockupbilder/motionzone.jpg",
     tech: "React, Next.js, TypeScript, Tailwind CSS, Shadcn, PostgreSQL, Prisma",
     projectLink: "https://dev.motionzoneworld.com/",
     codeLink: "https://github.com/motionzoneproject/motionzoneproject",
   },
 
   fakeyNails: {
-    image: "/Image/mockupbilder/fakeynails.jpg",
+    image: "/image/mockupbilder/fakeynails.jpg",
     tech: "React, Next.js, Sanity CMS, TypeScript, TailwindCSS, Shadcn, Nodmailer",
     codeLink: "https://github.com/knixan/fakey-nails",
     projectLink: "https://fakey-nails.vercel.app/",
   },
   dagensDons: {
-    image: "/Image/mockupbilder/dagensdos.jpg",
+    image: "/image/mockupbilder/dagensdos.jpg",
     tech: "React, Next.js, TypeScript, Tailwind CSS, Shadcn, PostgreSQL, Prisma, Zod, BetterAuth",
     codeLink: "https://github.com/knixan/dagens-dos",
   },
 
-  kladerEfterVader: {
-    image: "/Image/mockupbilder/vaderefterklader.jpg",
-    tech: "React, Next.js, TypeScript, Tailwind CSS, OpenWeatherMap API",
-    projectLink: "https://vaderefterklader.se",
-    codeLink: "https://github.com/knixan/vaderefterklader",
-  },
   skeningeVvs: {
-    image: "/Image/mockupbilder/skenningevvs.jpg",
+    image: "/image/mockupbilder/skenningevvs.jpg",
     tech: "React, Next.js, TypeScript, Tailwind CSS",
     projectLink: "https://skeningevvs.se/",
     codeLink: "https://github.com/knixan/skeningevvsab",
   },
   vaderskuggan: {
-    image: "/Image/mockup-vaderskuggan.png",
+    image: "/image/mockup-vaderskuggan.png",
     tech: "React, Next.js, TypeScript, Tailwind CSS, API",
     projectLink: "https://vaderskuggan.vercel.app/",
     codeLink: "https://github.com/knixan/vaderskuggan",
   },
   studenthubben: {
-    image: "/Image/mockup-studenthubben.png",
+    image: "/image/mockup-studenthubben.png",
     tech: "React, Next.js, TypeScript, Material UI",
     projectLink: "https://studenthubben-six.vercel.app/",
     codeLink: "https://github.com/knixan/studenthubben",
   },
   gammelBygg: {
-    image: "/Image/mockupbilder/gammelbygg-mockup.jpg",
+    image: "/image/mockupbilder/gammelbygg-mockup.jpg",
     tech: "React, Next.js, TypeScript, Tailwind CSS",
     projectLink: "https://gammelbyggnadsmaleri.vercel.app/",
     codeLink: "https://github.com/knixan/gammelbyggnadsmaleri",
   },
   ledningsteknik: {
-    image: "/Image/mockupbilder/ledningsteknink-mockup.jpg",
+    image: "/image/mockupbilder/ledningsteknink-mockup.jpg",
     tech: "React, Next.js, TypeScript, Tailwind CSS",
     projectLink: "https://ledningsteknik.vercel.app/",
     codeLink: "https://github.com/knixan/ledningsteknik",
   },
   jonnyEriksson: {
-    image: "/Image/mockupbilder/jonnyeriksson-mockup.jpg",
+    image: "/image/mockupbilder/jonnyeriksson-mockup.jpg",
     tech: "React, Next.js, TypeScript, Tailwind CSS",
     projectLink: "https://jonnyeriksson.vercel.app/",
     codeLink: "https://github.com/knixan/jonnyeriksson-ART",
