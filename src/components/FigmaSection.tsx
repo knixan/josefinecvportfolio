67c12c47a6f4a7ce4e605ee8498f298383453af0
Josefine <josefineeriksson@live.se>
2026-07-15 00:19:18 +0200

    Tassa in figma i portfolio

--- src/components/FigmaSection.tsx
+++ src/components/FigmaSection.tsx
@@ -7,11 +7,11 @@ const FIGMA_EMBEDS = [
     src: "https://embed.figma.com/design/L7Vwr7ypRMxuGrZfo3o5bC/Skeninge-VVS?node-id=0-1&embed-host=share",
     titleKey: "figma.design2.title",
   },
-  {
-    src: "https://embed.figma.com/design/fjj2iJsqWaueN5rBOvPs1c/Untitled?node-id=0-1&embed-host=share",
-    titleKey: "figma.design1.title",
-  },
 
+  {
+    src: "https://embed.figma.com/design/JE8FdREtjy44nce6vVGego/Untitled?node-id=0-1&embed-host=share",
+    titleKey: "figma.design3.title",
+  },
 ];
 
 export default function FigmaSection() {
